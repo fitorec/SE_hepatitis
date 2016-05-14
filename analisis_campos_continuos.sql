0df001022a021ecee471782ea49e5d3dc000f621
Fitorec <chanerec@gmail.com>
2016-05-14 07:29:41 -0500

    Convirtiendo el archivo de analisís de texto a markdown

--- analisis_campos_continuos.sql
+++ analisis_campos_continuos.sql
@@ -7,13 +7,16 @@
 --
 
 SELECT "Mostrando datos estadisticos referente a la edad";
+SELECT @muestra := COUNT(id) FROM `paciente` WHERE edad IS NOT NULL;
 
 SELECT
 	REPLACE(REPLACE(estado, 1, 'muerto'), 2, 'vivo') AS estado,
-	SUM(edad) AS suma_edad,	
+	COUNT(id)/@muestra AS porcentaje,
 	COUNT(id) AS muestras,
+	SUM(edad) AS suma,
 	AVG(edad) AS promedio,
-	VARIANCE(edad) varianza_estandar
+	STD(edad) desviacion_estandar,
+	VARIANCE(edad) varianza_estandar	
 FROM `paciente` WHERE 1
 GROUP BY estado
 ORDER BY estado DESC;
